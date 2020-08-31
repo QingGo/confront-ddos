a4412d2220f19181353a674873b9c9ed1a6aa20f
清歌 <zyqingjohn@qq.com>
2020-08-31 23:07:38 +0800

    实现自动断网脚本

---
+++ README.md
@@ -0,0 +1,4 @@
+只要我网线拔得够快，ddos就打不到我。使用方法：
+```
+nohup ./stop_net_when_ddos.sh eth0 > stop_net_when_ddos.log 2>&1 &
+```
---
+++ stop_net_when_ddos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 第一个参数为要处理的网卡名，如eth0
+# 使用方法 
+# sudo su
+# nohup ./stop_net_when_ddos.sh eth0 > stop_net_when_ddos.log 2>&1 &
+
+while true
+do
+    RX_pre=$(cat /proc/net/dev | grep $1 | sed 's/:/ /g' | awk '{print $2}')
+    sleep 1
+    RX_next=$(cat /proc/net/dev | grep $1 | sed 's/:/ /g' | awk '{print $2}')
+    RX=$((${RX_next}-${RX_pre}))
+    # 入流量大于5Mbit，断网5min
+    if [[ $RX -gt 655360 ]];then
+        echo "stop network interface $1 at `date`"
+        ifconfig $1 down
+        sleep 300
+        echo "start network interface $1 at `date`"
+        ifconfig $1 up
+    fi
+    # if [[ $RX -lt 1024 ]];then
+    # RX="${RX}B/s"
+    # elif [[ $RX -gt 1048576 ]];then
+    # RX=$(echo $RX | awk '{print $1/1048576 "MB/s"}')
+    # else
+    # RX=$(echo $RX | awk '{print $1/1024 "KB/s"}')
+    # fi
+    # echo -e "$ethn \t $RX"
+done
